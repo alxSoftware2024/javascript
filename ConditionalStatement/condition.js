2aa58c8bbf835f4589b579a0ac9753126497ebe1
alxsoftware2024 <ashenafin2024@gmail.com>
2024-10-02 10:46:42 -0700

    Break in loop

--- ConditionalStatement/condition.js
+++ ConditionalStatement/condition.js
@@ -1,4 +1,18 @@
 let name=['ashenafi','bixa','abi','estif'];
-if(name.length>3){
+if(name.length>5){
     console.log("more member");
+}
+else{
+    console.log('correct');
+}
+//break
+const score=[50,25,0,30,100,20,10];
+for(let i=0;i<score.length;i++)
+{
+    console.log("Your score"+' '+score[i]);
+    if(score[i]==100)
+    {
+        console.log("Congra you get 100");
+        break;
+    }
 }
