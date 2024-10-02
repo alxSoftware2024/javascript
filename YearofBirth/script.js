21d509591928fe8fc10199e502f70b522fd70df6
alxsoftware2024 <ashenafin2024@gmail.com>
2024-10-02 05:45:36 -0700

    script file

---
+++ YearofBirth/script.js
@@ -0,0 +1,5 @@
+let userName=prompt("please enter your name");
+let userAge=prompt("please enter your Age");
+let currentYear=new Date().getFullYear();
+let year_Of_Birth=currentYear-userAge;
+alert("Name"+' '+userName+' '+"Age"+' '+userAge);
