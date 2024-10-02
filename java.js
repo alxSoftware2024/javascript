66ede72b8108419fe2dee839736bc57d1ed694b9
alxsoftware2024 <ashenafin2024@gmail.com>
2024-10-01 20:18:52 -0700

    Variable

--- java.js
+++ java.js
@@ -1,1 +1,12 @@
-alert("hello,world");
+//JAVASCRIPT VARIABLE
+let age=30;
+let year=2020;
+console.log(age,year);
+// variable create by const keyword never change
+
+const value=200;
+console.log(value);
+
+//crate using var
+var score=900;
+console.log(score);
