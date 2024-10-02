d8e04041eb3b5c15e7493a01af8e208fc7d99459
alxsoftware2024 <ashenafin2024@gmail.com>
2024-10-02 05:47:38 -0700

    year of birth

--- YearofBirth/script.js
+++ YearofBirth/script.js
@@ -2,4 +2,4 @@ let userName=prompt("please enter your name");
 let userAge=prompt("please enter your Age");
 let currentYear=new Date().getFullYear();
 let year_Of_Birth=currentYear-userAge;
-alert("Name"+' '+userName+' '+"Age"+' '+userAge);
+alert("Name"+' '+userName+' '+"Age"+' '+year_Of_Birth);
