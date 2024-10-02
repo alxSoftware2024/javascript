3d5e04a30f7c59024ee00ce310210ead5ee617e5
alxsoftware2024 <ashenafin2024@gmail.com>
2024-10-02 09:57:19 -0700

    js file

---
+++ loop/loop.js
@@ -0,0 +1,10 @@
+//For loop
+for(let i=0;i<5;i++)
+{
+    console.log("in Loop"+' '+i);
+}
+console.log("Loop finshed");
+const name=["ASHENAFI","KENBON","FIKRU","KENA"];
+for(let j=0; j<name.length;j++){
+    console.log(name[j]);
+}
