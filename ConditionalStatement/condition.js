37cae7891c989a21706f21f04125b3f98c36fb4b
alxsoftware2024 <ashenafin2024@gmail.com>
2024-10-02 10:51:24 -0700

    continue keyword

--- ConditionalStatement/condition.js
+++ ConditionalStatement/condition.js
@@ -9,7 +9,12 @@ else{
 const score=[50,25,0,30,100,20,10];
 for(let i=0;i<score.length;i++)
 {
+    if(score[i]==0)
+    {
+        continue;
+    }
     console.log("Your score"+' '+score[i]);
+  
     if(score[i]==100)
     {
         console.log("Congra you get 100");
