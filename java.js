03e828e34c88a8196312b1bac7cddc208b983b40
alxsoftware2024 <ashenafin2024@gmail.com>
2024-10-01 20:51:20 -0700

    String

--- java.js
+++ java.js
@@ -10,3 +10,21 @@ console.log(value);
 //crate using var
 var score=900;
 console.log(score);
+//String 
+console.log("Helllo ,world");
+let email="ashenafin2024@gmail.com";
+console.log(email);
+//String concationation
+let firstName="Ashenafi";
+let lastName="Tesfaye";
+let fullName=firstName +''+ lastName;
+console.log(fullName);
+//Getting character
+console.log(fullName[0]);
+console.log(fullName[5]);
+//getting length
+console.log("The length of the your name is "+fullName.length);
+//method
+console.log(fullName.toUpperCase());
+console.log(fullName.toLowerCase());
+console.log(fullName.indexOf('A'));
