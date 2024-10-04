14d7656f563b153feddfaad3d451b0c969361c4f
alxsoftware2024 <ashenafin2024@gmail.com>
2024-10-03 21:39:52 -0700

    Get element by tag name

--- event/event.js
+++ event/event.js
@@ -18,7 +18,12 @@ console.log(error);
 //Get element by ID
 const title=document.getElementById('page-title');
 console.log(title);
-*/
+
 //GET element by class name
 const getbyclass=document.getElementsByClassName('error');
 console.log(getbyclass);
+*/
+//Get element by tag name
+const para4=document.getElementsByTagName('p');
+console.log(para4);
+console.log(para4[0]);
