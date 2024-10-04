b30c6f2d5880469d4789dae92d675c9a90cec529
alxsoftware2024 <ashenafin2024@gmail.com>
2024-10-03 21:10:15 -0700

    Query selector

---
+++ event/event.js
@@ -0,0 +1,8 @@
+//QuerySelector
+const para=document.querySelector('p');
+console.log(para);
+const para1=document.querySelector('.error');
+console.log(para1);
+//to select error class of <div>
+const para2=document.querySelector('div.error');
+console.log(para2);
