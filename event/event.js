a4d0c73f8c529add73520c88b1853055fc4b4846
alxsoftware2024 <ashenafin2024@gmail.com>
2024-10-03 21:19:20 -0700

    QuerySelectorAll

--- event/event.js
+++ event/event.js
@@ -6,3 +6,11 @@ console.log(para1);
 //to select error class of <div>
 const para2=document.querySelector('div.error');
 console.log(para2);
+//QuerySelectorAll
+const para3=document.querySelectorAll('p');
+para3.forEach(pa=>{
+    console.log(pa);
+});
+//select class of error
+const error=document.querySelectorAll('.error');
+console.log(error);
