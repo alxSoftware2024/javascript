c26a66abba9118e9d57e99b55f8b0efd0c2801cd
alxsoftware2024 <ashenafin2024@gmail.com>
2024-10-05 19:59:45 -0700

    Home page

--- event/eventex.js
+++ event/eventex.js
@@ -1,3 +1,9 @@
 //chage paragraph text
 const par=document.getElementById('greeting');
 par.textContent="Hello, DOM!";
+//Add an event listener to a button that changes the button’s text to “Clicked!” when it is clicked.
+const btn=document.getElementById('clickButton');
+btn.addEventListener('click' ,clicked);
+function clicked(eve){
+    btn.textContent="Clicked";
+}
