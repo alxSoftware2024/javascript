da793b2bb562635479bd3765649bf7bc87c34008
alxsoftware2024 <ashenafin2024@gmail.com>
2024-10-07 09:45:18 -0700

    push to Array List

--- array/array.js
+++ array/array.js
@@ -1,3 +1,10 @@
+//Display Array list
 let fruit=['banan','orange','mango','apple'];
-//Display Array list 
+//push method
+fruit.push('coconat');
 console.log(fruit);
+console.log(fruit[0]);
+console.log(fruit[1]);
+console.log(fruit[2]);
+console.log(fruit[3]);
+console.log(fruit[4]);
