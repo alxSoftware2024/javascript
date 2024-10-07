890d7c07aa2c20a24c9baf4572f95f1934634707
alxsoftware2024 <ashenafin2024@gmail.com>
2024-10-07 10:01:57 -0700

    Delete the last element

--- array/array.js
+++ array/array.js
@@ -1,10 +1,11 @@
 //Display Array list
 let fruit=['banan','orange','mango','apple'];
 //push method
-fruit.push('coconat');
+//fruit.push('coconat');
+//delete the last element
+fruit.pop();
 console.log(fruit);
 console.log(fruit[0]);
 console.log(fruit[1]);
 console.log(fruit[2]);
 console.log(fruit[3]);
-console.log(fruit[4]);
