9ec49770e7618967bcf1753d9a20d2731e64fbbc
alxsoftware2024 <ashenafin2024@gmail.com>
2024-10-16 19:03:43 -0700

    delete element 'p'

--- node/node.js
+++ node/node.js
@@ -3,3 +3,5 @@ const node=document.createTextNode("This is the new paragraph");
 para.appendChild(node);
 const element=document.getElementById('div1');
 element.appendChild(para);
+const remove=document.getElementById('p1');
+remove.remove();
