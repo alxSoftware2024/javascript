237e224486a06b8fa6439abd474a33a005d1af87
alxsoftware2024 <ashenafin2024@gmail.com>
2024-10-17 20:08:32 -0700

    convert json to js format

--- json/jsonn.js
+++ json/jsonn.js
@@ -2,3 +2,6 @@
 const user={name:"ashenafi",age:23};
 const jsonuser=JSON.stringify(user);
 console.log(jsonuser);
+//JSON.parse(jsonString): Parses a JSON string and returns a JavaScript object.
+const jsobject=JSON.parse(jsonuser);
+console.log(jsobject);
