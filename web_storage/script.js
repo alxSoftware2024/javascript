178b52cf08f9859ccaa336a90e7683a6f1e41210
alxsoftware2024 <ashenafin2024@gmail.com>
2024-10-17 20:34:58 -0700

    sessionStorage

--- web_storage/script.js
+++ web_storage/script.js
@@ -3,3 +3,9 @@ localStorage.setItem('username','ashtes');
 // Retrieving data
 const username=localStorage.getItem('username');
 console.log(username);
+//Session storage
+//storing data
+sessionStorage.setItem('cart',JSON.stringify({item:"prodact",quantity:2}));
+//Retrieving data
+const cart=JSON.parse(sessionStorage.getItem('cart'));
+console.log(cart);
